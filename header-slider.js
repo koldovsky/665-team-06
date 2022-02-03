4e9df84955750ea6bbef2db44077e3b88b7521dd
YaroslavYashchenko <yaris2804@gmail.com>
2022-02-03 18:24:21 +0200

    makes images in slider responsible

--- header-slider.js
+++ header-slider.js
@@ -3,23 +3,26 @@
         `<div class="header__header-bottom__image__slider">
             <img src="img/header/notebook-header.png" alt="notebook header">
         </div>`,
-        `<div class="handmade__header-img-man">
-            <img src="img/bloc_handmade/man_with_a_bag.webp" alt="man with a bag">
-        </div>`,
-        `<div class="handmade__img-man-with-a-bag-on-his-shoulder">
-            <img src="img/bloc_handmade/man_with-a_bag-on-his_shoulder.webp" alt="man with a bag on his shoulder">
-        </div>`,
-        `<div class="handmade__img-gloves">
+        `<div class="header__header-bottom__image__slider">
             <img src="img/bloc_handmade/open_bag.webp" alt="open bag">
         </div>`,
-        `<div class="urban_pack">
+        `<div class="header__header-bottom__image__slider">
             <img src="img/gift_packs/urban_pack.webp" alt="urban pack">
         </div>`,
-        `<div class="vintage_pack">
+        `<div class="header__header-bottom__image__slider">
+            <img src="img/collection/wallets-collectins.webp" alt="walets collection">
+        </div>`,
+        `<div class="header__header-bottom__image__slider">
             <img src="img/gift_packs/vintage_pack.webp" alt="vintage pack">
         </div>`,
-        `<div class="summer_pack">
+        `<div class="header__header-bottom__image__slider">
+            <img src="img/collection/cases-collections.webp" alt="cases collections">
+        </div>`,
+        `<div class="header__header-bottom__image__slider">
             <img src="img/gift_packs/summer_pack.webp" alt="summer_pack_foto">
+        </div>`,
+        `<div class="header__header-bottom__image__slider">
+            <img src="img/collection/holders-collections.webp" alt="holders collections">
         </div>`
     ];
 
@@ -33,6 +36,6 @@
         showCurrenSlide();
     }
     showCurrenSlide();
-    setInterval(showNextSlide, 2000);
+    setInterval(showNextSlide, 3000);
 
 })();
