90cb1008897747e9dc3676a0222b4923c33cf435
VoyatAlex <alexandrvoyat@gmail.com>
2022-02-03 09:05:32 +0000

    made js file

---
+++ prodakt-list.js
@@ -0,0 +1,89 @@
+(function set(){
+    document.getElementById('inner').innerHTML=`<div class="row row-cols-1 row-cols-md-3 g-4">
+    <div class="col">
+      <div class="card">
+        <img src="img/bags/Handmade-Leather-Notebook.jpg" class="card-img-top" alt="Handmade Leather Notebook">
+        <div class="card-body">
+          <h5 class="card-title">Handmade Leather Notebook</h5>
+          <div>
+          <p class="card-text">$89,00</p>
+          </div>
+        </div>
+        <div class="text-center">
+            <a href="#" class="card-link">Buy</a>
+         </div>
+      </div>
+    </div>
+    <div class="col">
+        <div class="card">
+          <img src="img/bags/Brown-Leather-Wallet.jpg" class="card-img-top" alt="Brown Leather Wallet">
+          <div class="card-body">
+            <h5 class="card-title">Brown Leather Wallet</h5>
+            <div>
+            <p class="card-text">$76,00</p>
+            </div>
+          </div>
+          <div class="text-center">
+              <a href="#" class="card-link">Buy</a>
+           </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card">
+          <img src="img/bags/Brown-Business-Folder.jpg" class="card-img-top" alt="Brown Business Folder">
+          <div class="card-body">
+            <h5 class="card-title">Brown Business Folder</h5>
+            <div>
+            <p class="card-text">$94,00</p>
+            </div>
+          </div>
+          <div class="text-center">
+              <a href="#" class="card-link">Buy</a>
+           </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card">
+          <img src="img/bags/Big-Brown-Leather-Bag.jpg" class="card-img-top" alt="Big Brown Leather Bag">
+          <div class="card-body">
+            <h5 class="card-title">Big Brown Leather Bag</h5>
+            <div>
+            <p class="card-text">$124,00</p>
+            </div>
+          </div>
+          <div class="text-center">
+              <a href="#" class="card-link">Buy</a>
+           </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card">
+          <img src="img/bags/Stylish-Brown-Briefcase.jpg" class="card-img-top" alt="Stylish Brown Briefcase">
+          <div class="card-body">
+            <h5 class="card-title">Stylish Brown Briefcase</h5>
+            <div>
+            <p class="card-text">$200,00</p>
+            </div>
+          </div>
+          <div class="text-center">
+              <a href="#" class="card-link">Buy</a>
+           </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card">
+          <img src="img/bags/Three-Color-Leather-Purse.jpg" class="card-img-top" alt="Three-Color Leather Purse">
+          <div class="card-body">
+            <h5 class="card-title">Three-Color Leather Purse</h5>
+            <div>
+            <p class="card-text">$90,00</p>
+            </div>
+          </div>
+          <div class="text-center">
+              <a href="#" class="card-link">Buy</a>
+           </div>
+        </div>
+      </div>
+  </div>`;
+   })()
+  
