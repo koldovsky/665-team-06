e1bc509328bb16b8b7003e34010d8cef40663ed9
Daniel <danyakhro@icloud.com>
2022-02-14 02:37:45 +0200

    added page

---
+++ js/custom-orders.js
@@ -0,0 +1,23 @@
+document.getElementById("custom-orders").innerHTML = ` 
+      <div class="overblock">
+<div class="overblock__header">
+    <div class="overblock__header-text">
+        <h1 style="font-size: 41px";>Custom Orders</h1>
+        
+    </div>
+</div>
+<div class="overblock__main">
+    <p class="overblock__main-text">
+        <span class="overblock__main-text-content">
+            Although a quality leather product is a wonderful gift by itself, adding a touch of
+            customization
+            turns it into an even better one. Whether you are looking to provide your company with
+            elegant and
+            unified corporate accessories, looking for personalized gifts for your loved ones, or just
+            want to
+            treat your self to unique leather goods, our custom branding option is the perfect way to
+            go.
+        </span>
+    </p>
+</div>
+</div>`;
