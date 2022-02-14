482f074a273006213c1e645d92977bd0e3da7fb1
YaroslavYashchenko <yaris2804@gmail.com>
2022-02-14 21:49:06 +0200

    fixed margins in header

--- js/text-typing.js
+++ js/text-typing.js
@@ -2,7 +2,7 @@ const TxtRotate = function (el, toRotate, period) {
     this.toRotate = toRotate;
     this.el = el;
     this.loopNum = 0;
-    this.period = parseInt(period, 10) || 2000;
+    this.period = parseInt(period, 10) || 1500;
     this.txt = '';
     this.tick();
     this.isDeleting = false;
